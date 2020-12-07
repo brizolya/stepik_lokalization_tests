ef840ce328e35dea2621a05424006b074a87fb3a
Olga <73897007+brizolya@users.noreply.github.com>
2020-12-07 21:32:36 +0100

    error correction

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -9,12 +10,14 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="function")
 def browser(request):
-    language = request.config.getoption("language")
+    language = str(request.config.getoption("--language"))
 
     print(f"\nstart browser at {language}-language for test..")
     
     options = Options()
+    #options.add_argument("-lang="+ language)
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    #print(options.arguments)
     browser = webdriver.Chrome(options=options)
 
     yield browser
--- test_items.py
+++ test_items.py
@@ -1,12 +1,13 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import time
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_check_button_add_to_cart(browser):
     browser.get(link)
-    button = WebDriverWait(browser, 5).until( EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn-add-to-basket")))
+    time.sleep(10)
+    button = browser.find_elements_by_css_selector("button.btn-add-to-basket")
+    assert len(button) == 1, "Only one button expected!"
+    
 
 
     
